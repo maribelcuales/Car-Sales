454f5168251778b0efaafb939d212fcf48f26046
maribelcuales <maribelcuales@gmail.com>
2020-07-21 15:02:48 +0800

    Added an onClick handler passing in the addNewFeature props.

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -5,19 +5,10 @@ const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={() => props.addNewFeature(props.feature)} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    feature: state.features
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  {}
-)(AdditionalFeature);
+export default AdditionalFeature;
