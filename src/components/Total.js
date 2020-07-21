3e22e4d86ea03d23729062d1f3ac53697c46fa9f
maribelcuales <maribelcuales@gmail.com>
2020-07-21 15:15:42 +0800

    Corrected error in props.car.price and updated it to props.price.

--- src/components/Total.js
+++ src/components/Total.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.price + props.additionalPrice}</h4>
     </div>
   );
 };
