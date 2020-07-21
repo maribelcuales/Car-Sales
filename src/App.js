036ca0b7c77358c745b7369eccbffb27462cd5d1
maribelcuales <maribelcuales@gmail.com>
2020-07-21 15:11:57 +0800

    Deleted props in the Total component since its state is already connected to the Redux store.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       </div>
       <div className="box">
         <AdditionalFeatures />
-        {/* <Total car={state.car} additionalPrice={state.additionalPrice} /> */}
+        <Total />
       </div>
     </div>
   );
